d88fde2724d99be6a06ee45e87227fabb19217d5
mcbarinov <mcbarinov@proton.me>
2025-05-21 13:54:16 +0300

    improve cli account deposit-address output

--- src/mm_okx/cli/commands/account/deposit_address.py
+++ src/mm_okx/cli/commands/account/deposit_address.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from mm_std import print_json
+from mm_std import fatal, print_table
 
 from mm_okx.clients.account import AccountClient, AccountConfig
 
@@ -8,4 +8,11 @@ from mm_okx.clients.account import AccountClient, AccountConfig
 async def run(account: Path, ccy: str) -> None:
     client = AccountClient(AccountConfig.from_toml_file(account))
     res = await client.get_deposit_address(ccy)
-    print_json(res)
+    if res.is_err():
+        fatal(res.unwrap_error())
+
+    headers = ["ccy", "chain", "address"]
+
+    rows = [[a.ccy, a.chain, a.addr] for a in res.unwrap()]
+
+    print_table(title="Deposit Address", columns=headers, rows=rows)
